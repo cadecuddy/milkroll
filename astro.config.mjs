8e04bafee615015771ac76f0a9f83bc5ff32468f
Cade Cuddy <cadecuddy@gmail.com>
2023-01-15 15:37:53 -0500

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+import { remarkReadingTime } from './remark-reading-time.mjs';
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://blog.cadecuddy.com',
+  integrations: [tailwind({}), mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    extendDefaultPlugins: true
+  },
+});
